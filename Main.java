1c8c2a229d821eda125464c88446aa46218a4554
Clahoz20 <73487890+Clahoz20@users.noreply.github.com>
2021-10-11 17:21:45 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jaumebalmes.personesxml;
+
+/**
+ *
+ * @author claud
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Persona llista[] = new Persona(4);
+        
+        llista[0] = new Persona("Pedro", "Martínez", 26, 664485132);
+        llista[1] = new Persona("Miguel", "Navarro", 43, 651874523);
+        llista[2] = new Persona("Jordina", "Pujol", 35, 648153244);
+        llista[3] = new Persona("Gerard", "Piqué", 36, 618475551);
+        
+        Filtre temp[] = new Filtre(4);
+        
+        for (int i = 0; i < 4; i++) 
+            temp[i] = new Filtre(llista[i]);
+        
+        
+    }
+    
+}
